408fd6f070822f763fafa246771f1db385c57fbd
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2023-10-20 19:56:35 +0100

    Updated the app functions

--- script.js
+++ script.js
@@ -4,12 +4,89 @@ let searchInput = $("#search-input");
 let searchForm = $("#search-form");
 let searchHistory = [];
 let searchHistoryContainer = $("#history");
+let todayContainer = $("#today");
 const weatherAPIURL = "https://api.openweathermap.org";
 const weatherAPIKey = "f6e9363350029ec2bfab3e53505ac9c5";
 //
 //
 // -------------------  Function setup  -------------------------------
 //
+function renderSearchHistory() {
+  searchHistoryContainer.html("");
+
+  for (let i = 0; i < searchHistory.length; i++) {
+    let btn = $("<button>");
+    btn.attr("type", "button");
+    btn.addClass("history-btn btn-history");
+    btn.attr("data-search", searchHistory[i]);
+    btn.text(searchHistory[i]);
+    searchHistoryContainer.append(btn);
+  }
+}
+
+function appendSearchHistory(search) {
+  if (searchHistory.indexOf(search) !== -1) {
+    return;
+  }
+  searchHistory.push(search);
+  localStorage.setItem("search-history", JSON.stringify(searchHistory));
+  renderSearchHistory();
+}
+
+function renderCurrentWeather(city, weatherData) {
+  let date = moment().format("D/M/YYYY");
+  let tempC = weatherData["main"]["temp"];
+  let windKph = weatherData["wind"]["speed"];
+  let humidity = weatherData["main"]["humidity"];
+
+  let iconUrl = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  let iconDescription =
+    weatherData.weather[0].description || weatherData[0].main;
+
+  let card = $("<div>");
+  let cardBody = $("<div>");
+  let weatherIcon = $("<img>");
+  let heading = $("<h2>");
+  let tempEl = $("<p>");
+  let windEl = $("<p>");
+  let humidityEl = $("<p>");
+
+  card.attr("class", "card");
+  cardBody.attr("class", "card-body");
+  card.append(cardBody);
+  heading.attr("class", "h3 card-title");
+  tempEl.attr("class", "card-text");
+  windEl.attr("class", "card-text");
+  humidityEl.attr("class", "card-text");
+  heading.text(`${city} (${date})`);
+  weatherIcon.attr("src", iconUrl);
+  weatherIcon.attr("alt", iconDescription);
+  heading.append(weatherIcon);
+  tempEl.text(`Temp ${tempC} C`);
+  windEl.text(`Wind ${windKph} KPH`);
+  humidityEl.text(`Humidity ${humidity} %`);
+  cardBody.append(heading, tempEl, windEl, humidityEl);
+  todayContainer.html("");
+  todayContainer.append(card);
+}
+
+function fetchWeather(location) {
+  let latitude = location.lat;
+  let longitude = location.lon;
+  let city = location.name;
+  let queryWeatherURL = `${weatherAPIURL}/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${weatherAPIKey}`;
+  console.log(queryWeatherURL);
+
+  //
+  $.ajax({
+    url: queryWeatherURL,
+    method: "GET",
+  }).then(function (response) {
+    renderCurrentWeather(city, response.list[0]);
+    // renderForecast(data.list);
+  });
+}
+
 function fetchCoordinates(search) {
   let queryURL = `${weatherAPIURL}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherAPIKey}`;
 
@@ -21,27 +98,26 @@ function fetchCoordinates(search) {
       if (!response[0]) {
         alert("Location not found");
       } else {
-        if (searchHistory.indexOf(search) !== -1) {
-          return;
-        }
-        searchHistory.push(search);
-        localStorage.setItem("search-history", JSON.stringify(searchHistory));
-        searchHistoryContainer.html("");
-
-        for (let i = 0; i < searchHistory.length; i++) {
-          let btn = $("<button>");
-          btn.attr("type", "button");
-          btn.addClass("history-btn btn-history");
-          btn.attr("data-search", searchHistory[i]);
-        }
+        appendSearchHistory(search);
+        fetchWeather(response[0]);
       }
     });
+}
+
+function initializeHistory() {
+  let storedHistory = localStorage.getItem("search-history");
+  if (storedHistory) {
+    searchHistory = JSON.parse(storedHistory);
+  }
+  renderSearchHistory();
 }
 
 function submitSearchForm(event) {
   event.preventDefault();
   let search = searchInput.val().trim();
   fetchCoordinates(search);
+  searchInput.val("");
 }
 
+initializeHistory();
 searchForm.on("submit", submitSearchForm);
